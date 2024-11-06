d283ddb7f0c7f90b096a8f5a0795856854e381a1
yeong <chlgkdud111@gmail.com>
2024-11-06 21:13:31 +0900

    응답 전송을 위한 ResponseDTO 파일 만듦

---
+++ src/main/java/com/example/baseball/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.baseball.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO<T> {
+	
+	private int status;
+	private T data;
+	
+}
